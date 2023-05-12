d6cbaef133ed6a476194e369e529a67309a570a1
azolanda <uboikov@yandex.ru>
2023-05-12 06:55:25 +0300

    task 4

--- seminar2/Exceptions002.java
+++ seminar2/Exceptions002.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 
 public class Exceptions002 {
     public static void main(String[] args) {
-        // System.out.println(getFloatNum());
+        Scanner scan = new Scanner(System.in);
+        // System.out.println(getFloatNum(scan));
 
         int[] array = { 1, 2, 3, 3, 3, 2, 1 };
-        task2(array);
-
+        // task2(array);
+        System.out.println("Введенная строка: \n" + getStr(scan));
+        scan.close();
     }
 
     // 1. Реализуйте метод, который запрашивает у пользователя ввод дробного числа
@@ -18,21 +20,19 @@ public class Exceptions002 {
     // к падению приложения, вместо этого, необходимо повторно запросить у
     // пользователя ввод данных.
 
-    public static Float getFloatNum() {
+    public static Float getFloatNum(Scanner sc) {
         Float num = null;
-        Scanner scan = new Scanner(System.in);
 
         do {
             try {
                 System.out.println("Введите число: --> ");
-                num = scan.nextFloat();
+                num = sc.nextFloat();
             } catch (InputMismatchException e) {
                 System.out.println("Ввод в неверном формате! Повторите ввод");
-                scan.nextLine();
+                sc.nextLine();
             }
         } while (num == null);
 
-        scan.close();
         return num;
     }
 
@@ -51,4 +51,16 @@ public class Exceptions002 {
     // 4. Разработайте программу, которая выбросит Exception,
     // когда пользователь вводит пустую строку.
     // Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+    public static String getStr(Scanner sc) {
+        String str = null;
+        System.out.println("Введите строку: -->");
+        str = sc.nextLine();
+
+        if (str.equals("")) {
+            throw new RuntimeException("Пустые строки вводить нельзя!");
+        }
+
+        return str;
+    }
 }
